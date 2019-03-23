08ab96a473a6bc66b49666377ec0edd6fc6af776
Serena Berry <siberry226@gmail.com>
2019-03-23 08:37:20 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140313192430) do
+
+  create_table "boat_classifications", force: :cascade do |t|
+    t.integer  "boat_id"
+    t.integer  "classification_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "boats", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "length"
+    t.integer  "captain_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "captains", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "admiral"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "classifications", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
